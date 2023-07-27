7b02eaf1c4bdfd0a31cd684ec8e9bb0a90504534
soulduse <soulduse@naver.com>
2023-07-28 08:21:44 +0900

    자바코드 구성정보 사용

--- toby-spring-boot/src/main/kotlin/com/example/inflearnspring/HelloController.kt
+++ toby-spring-boot/src/main/kotlin/com/example/inflearnspring/HelloController.kt
@@ -1,16 +1,14 @@
 package com.example.inflearnspring
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping
+@RestController
 class HelloController(
     private val helloService: HelloService,
 ) {
 
     @GetMapping("/hello")
-    @ResponseBody
     fun hello(name: String): String {
         return helloService.hello(name)
     }
--- toby-spring-boot/src/main/kotlin/com/example/inflearnspring/InflearnSpringApplication.kt
+++ toby-spring-boot/src/main/kotlin/com/example/inflearnspring/InflearnSpringApplication.kt
@@ -2,21 +2,42 @@ package com.example.inflearnspring
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.server.WebServer
-import org.springframework.context.support.registerBean
-import org.springframework.web.context.support.GenericWebApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
 import org.springframework.web.servlet.DispatcherServlet
 
-fun main(args: Array<String>) {
-    val applicationContext = GenericWebApplicationContext().apply {
-        registerBean<SimpleHelloService>()
-        registerBean<HelloController>()
-        refresh()
+@Configuration
+class InflearnSpringApplication {
+    @Bean
+    fun helloController(helloService: HelloService): HelloController {
+        return HelloController(helloService)
     }
 
-    val serverFactory = TomcatServletWebServerFactory()
-    val webServer: WebServer = serverFactory.getWebServer({ servletContext ->
-        servletContext.addServlet("dispatcherServlet", DispatcherServlet(applicationContext))
-            .addMapping("/*")
-    })
-    webServer.start()
+    @Bean
+    fun helloService(): HelloService {
+        return SimpleHelloService()
+    }
+}
+
+fun main(args: Array<String>) {
+    val applicationContext = object : AnnotationConfigWebApplicationContext() {
+        @Suppress("ACCIDENTAL_OVERRIDE")
+        override fun setClassLoader(classLoader: ClassLoader) {
+            this.classLoader = classLoader
+        }
+
+        override fun onRefresh() {
+            super.onRefresh()
+            val serverFactory = TomcatServletWebServerFactory()
+            val webServer: WebServer = serverFactory.getWebServer({ servletContext ->
+                servletContext.addServlet("dispatcherServlet", DispatcherServlet(this))
+                    .addMapping("/*")
+            })
+            webServer.start()
+        }
+    }.apply {
+        register(InflearnSpringApplication::class.java)
+        refresh()
+    }
 }
