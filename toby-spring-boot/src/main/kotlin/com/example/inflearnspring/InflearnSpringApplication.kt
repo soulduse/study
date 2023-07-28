7436ec0445fbd96cd183de03a2200bd1298aa7a4
soulduse <soulduse@naver.com>
2023-07-28 10:00:10 +0900

    코드정리

--- toby-spring-boot/src/main/kotlin/com/example/inflearnspring/InflearnSpringApplication.kt
+++ toby-spring-boot/src/main/kotlin/com/example/inflearnspring/InflearnSpringApplication.kt
@@ -1,12 +1,12 @@
 package com.example.inflearnspring
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.runApplication
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.server.WebServer
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
 import org.springframework.web.servlet.DispatcherServlet
 
 @Configuration
@@ -24,25 +24,6 @@ class InflearnSpringApplication {
 }
 
 fun main(args: Array<String>) {
-    val applicationContext = object : AnnotationConfigWebApplicationContext() {
-        @Suppress("ACCIDENTAL_OVERRIDE")
-        override fun setClassLoader(classLoader: ClassLoader) {
-            this.classLoader = classLoader
-        }
-
-        override fun onRefresh() {
-            super.onRefresh()
-
-            val serverFactory = this.getBean(ServletWebServerFactory::class.java)
-            val dispatcherServlet = this.getBean(DispatcherServlet::class.java)
-            val webServer: WebServer = serverFactory.getWebServer({ servletContext ->
-                servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                    .addMapping("/*")
-            })
-            webServer.start()
-        }
-    }.apply {
-        register(InflearnSpringApplication::class.java)
-        refresh()
-    }
+    SpringApplication.run(InflearnSpringApplication::class.java, *args)
+//    runApplication<InflearnSpringApplication>(*args)
 }
