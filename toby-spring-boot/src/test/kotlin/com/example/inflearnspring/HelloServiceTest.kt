a478dbdf83142b27b7d4e1afae59ab0306b66df6
soulduse <soulduse@naver.com>
2023-07-29 10:05:32 +0900

    create Meta Annotations

--- toby-spring-boot/src/test/kotlin/com/example/inflearnspring/HelloServiceTest.kt
+++ toby-spring-boot/src/test/kotlin/com/example/inflearnspring/HelloServiceTest.kt
@@ -3,12 +3,32 @@ package com.example.inflearnspring
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.ANNOTATION_CLASS)
+@Test
+annotation class UnitTest
+
+@UnitTest
+annotation class FastUnitTest
+
 class HelloServiceTest {
-    @Test
+    @FastUnitTest
     fun simpleHelloService() {
         val helloService = SimpleHelloService()
         val ret = helloService.hello("Dave")
 
         assertThat(ret).isEqualTo("Hello, Dave")
+    }
+
+    @UnitTest
+    fun helloDecorator() {
+        val helloDecorator = HelloDecorator(object : HelloService {
+            override fun hello(name: String): String {
+                return "Hello, $name"
+            }
+        })
+        val ret = helloDecorator.hello("Dave")
+
+        assertThat(ret).isEqualTo("* Hello, Dave *")
     }
 }
