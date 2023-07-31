1474d491ec533bc1fbec0eb4565cf36aaab6a5a0
soulduse <soulduse@naver.com>
2023-07-31 15:50:30 +0900

    add DeferredImportSelector

--- toby-spring-boot/src/main/kotlin/com/example/config/MyAutoConfigImportSelector.kt
+++ toby-spring-boot/src/main/kotlin/com/example/config/MyAutoConfigImportSelector.kt
@@ -1,13 +1,14 @@
 package com.example.config
 
+import org.springframework.boot.context.annotation.ImportCandidates
 import org.springframework.context.annotation.DeferredImportSelector
 import org.springframework.core.type.AnnotationMetadata
 
-class MyAutoConfigImportSelector : DeferredImportSelector {
+class MyAutoConfigImportSelector(
+    private val classLoader: ClassLoader,
+) : DeferredImportSelector {
     override fun selectImports(importingClassMetadata: AnnotationMetadata): Array<String> {
-        return arrayOf(
-            "com.example.config.autoconfig.TomcatWebServerConfig",
-            "com.example.config.autoconfig.DispatcherConfig"
-        )
+        val candidates = ImportCandidates.load(MyAutoConfiguration::class.java, classLoader)
+        return candidates.toList().toTypedArray()
     }
 }
---
+++ toby-spring-boot/src/main/kotlin/com/example/config/MyAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package com.example.config
+
+import org.springframework.context.annotation.Configuration
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Configuration
+annotation class MyAutoConfiguration
